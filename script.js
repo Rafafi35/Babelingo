a5899349543ebc40e128c39b6abeeedd7052fdd6
Rafafi35 <salinger.rafael@gmail.com>
2025-08-18 14:42:59 +0200

    aufgaben als json & schreiben in der Lücke

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-const lückentext = document.getElementById("lückenText")
+const data = {
+    "aufgaben": [
+        {
+            "fremdSatz": "This place is wonderful!",
+            "lückenSatz": "Dieser Ort ist _!",
+            "lösungswort": "wunderbar"
+        },
+        {
+            "fremdSatz": "This is a once-in-a-lifetime oppertunity!",
+            "lückenSatz": "Dies ist eine einmalige _!",
+            "lösungswort": "Gelegenheit"
+        }
+    ]
+}
+const fremdTextAnzeige = document.getElementById("fremdText")
+const lückentextAnzeige = document.getElementById("lückenText")
 
 let loesungswort = ""
-document.addEventListener("keypress", function (event) {
+
+fremdTextAnzeige.textContent = data.aufgaben[0].fremdSatz
+let lückenText = data.aufgaben[0].lückenSatz
+lückentextAnzeige.textContent = lückenText
+let lückenTextTeile = lückenText.split("_")
+
+document.addEventListener("keydown", function (event) {
     loesungswort += event.key
-    lückentext.textContent = loesungswort
+    lückentextAnzeige.textContent = lückenTextTeile[0] + loesungswort + lückenTextTeile[1]
 
 })
